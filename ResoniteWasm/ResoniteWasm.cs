228a5f5206b2a61973d154758762de5d1ec21ec0
Phylliida <phylliida.dev@gmail.com>
2024-09-11 12:56:07 -0400

    in progress staging stuff here

--- ResoniteWasm/ResoniteWasm.cs
+++ ResoniteWasm/ResoniteWasm.cs
@@ -21,7 +21,129 @@ using System.IO;
 using Elements.Assets;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Bson;
-using WasiMinimalPolyfill;
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using Wasmtime;
+
+namespace WasiMinimalPolyfill2
+{
+    public class WasiFileSystem
+    {
+        // should return how many bytes written
+        public delegate int WriteDataHandler(System.Span<byte> dataWritten, int bytesWritten);
+        public event WriteDataHandler OnWriteStdOut;
+        public event WriteDataHandler OnWriteStdErr;
+
+        // should return how many bytes read
+        public delegate int ReadDataHandler(System.Span<byte> memoryToWriteTo, int bytesRequested);
+        public event ReadDataHandler OnReadStdIn;
+
+        public event WasmPolyfill.MemoryGrowthEvent OnMemoryGrowth;
+
+        public Wasmtime.Memory memory;
+
+        public Wasmtime.Instance instance;
+
+        static string FS_WRAPPER = "fs_wrapper";
+
+        public WasiFileSystem(Wasmtime.Engine engine, Wasmtime.Linker linker, Wasmtime.Store store, string fileSystemWasmPath)
+        {
+            Wasmtime.Module fileSystemModule = Wasmtime.Module.FromFile(engine, fileSystemWasmPath);
+
+            linker.Define(
+                FS_WRAPPER,
+                "read_stdin",
+                Function.FromCallback(store, (Int32 buf, Int32 len, Int64 offset) =>
+                {
+                    System.Span<byte> dataSpan = memory.GetSpan(buf, len + (Int32)offset).Slice((Int32)offset);
+                    return OnReadStdIn(dataSpan, len);
+                })
+            );
+
+            linker.Define(
+                FS_WRAPPER,
+                "write_stdout",
+                Function.FromCallback(store, (Int32 buf, Int32 len, Int64 offset) =>
+                {
+                    System.Span<byte> dataSpan = memory.GetSpan(buf, len + (Int32)offset).Slice((Int32)offset);
+                    return OnWriteStdOut(dataSpan, len);
+                })
+            );
+
+            linker.Define(
+                FS_WRAPPER,
+                "write_stderr",
+                Function.FromCallback(store, (Int32 buf, Int32 len, Int64 offset) =>
+                {
+                    System.Span<byte> dataSpan = memory.GetSpan(buf, len + (Int32)offset).Slice((Int32)offset);
+                    return OnWriteStdErr(dataSpan, len);
+                })
+            );
+
+            WasmPolyfill.AttachClockTimeExport(linker, store, () => memory, packageName: "wasi_snapshot_preview1");
+            WasmPolyfill.AttachMemoryGrowthExport(linker, store, OnMemoryGrowth);
+
+            instance = linker.Instantiate(store, fileSystemModule);
+
+            memory = instance.GetMemory("memory");
+
+            instance.GetFunction("_initialize").Invoke();
+        }
+
+
+
+    }
+
+    public class WasmPolyfill
+    {
+        public delegate Wasmtime.Memory GetMemory();
+
+        public delegate void MemoryGrowthEvent(Int32 growthAmount);
+        public static void AttachMemoryGrowthExport(Wasmtime.Linker linker, Wasmtime.Store store, MemoryGrowthEvent growthEvent = null)
+        {
+            linker.Define(
+                "env",
+                "emscripten_notify_memory_growth",
+                Function.FromCallback(store, (Int32 growthAmount) =>
+                {
+                    if (growthEvent != null)
+                    {
+                        growthEvent(growthAmount);
+                    }
+                })
+            );
+        }
+        private static long nanoTime()
+        {
+            long nano = 10000L * Stopwatch.GetTimestamp();
+            nano /= TimeSpan.TicksPerMillisecond;
+            nano *= 100L;
+            return nano;
+        }
+
+        // from https://stackoverflow.com/a/44136515
+        public static void AttachClockTimeExport(Wasmtime.Linker linker, Wasmtime.Store store, GetMemory memoryGetter, string packageName = "")
+        {
+            linker.Define(
+                packageName,
+                "clock_time_get",
+                Function.FromCallback(store, (Int32 clockId, Int64 precision, Int32 pointerToOutput) =>
+                {
+                    memoryGetter().WriteInt64(pointerToOutput, nanoTime());
+                    return (Int32)0;
+                })
+            );
+        }
+    }
+}
+
 
 namespace ResoniteEasyFunctionWrapperExampleMod
 {
@@ -509,12 +631,12 @@ namespace ResoniteEasyFunctionWrapperExampleMod
             linker = new Wasmtime.Linker(engine);
         }
 
-        public static WasiMinimalPolyfill.WasiFileSystem CreateFileSystem(Wasmtime.Linker linker, Wasmtime.Store store)
+        public static WasiMinimalPolyfill2.WasiFileSystem CreateFileSystem(Wasmtime.Linker linker, Wasmtime.Store store)
         {
-            return new WasiFileSystem(engine, linker, store, "C:/Users/yams/Desktop/prog/ResoniteWasm/memfs.wasm");
+            return new WasiMinimalPolyfill2.WasiFileSystem(engine, linker, store, "C:/Users/yams/Desktop/prog/ResoniteWasm/memfs.wasm");
         }
 
-        public static Wasmtime.Instance GetFileSystemInstance(WasiMinimalPolyfill.WasiFileSystem fileSystem)
+        public static Wasmtime.Instance GetFileSystemInstance(WasiMinimalPolyfill2.WasiFileSystem fileSystem)
         {
             return fileSystem.instance;
         }
